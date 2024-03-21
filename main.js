98cb6d4ae523b6e4709a670aedcf585d94ee0c8b
SebSpr <sebastian.spreitzer@uibk.ac.at>
2024-03-21 17:44:04 +0100

    add defer

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([46.816944, 11.250278], 13);
+var map = L.map('map').setView([-43.53, 170.15], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([46.816944, 11.250278]).addTo(map);
+var marker = L.marker([-43.53, 170.15]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
