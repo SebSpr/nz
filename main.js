9665eded0d310f892293dc636ad8dd7d5f850968
SebSpr <sebastian.spreitzer@uibk.ac.at>
2024-04-11 15:55:50 +0200

    var durch let ersetzt

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-var map = L.map('map').setView([-43.53, 170.15], 13);
+let map = L.map('map').setView([-43.53, 170.15], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([-43.53, 170.15]).addTo(map);
+let marker = L.marker([-43.53, 170.15]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
-let absatz = "Hallo Welt";
-let nummer = 6;
-console.log(`
-    <h3> Test für's "Tutorial" </h3>
-    <p> ${absatz}</p>
-    <p> Nummer plus 1 = ${nummer +1} </p>
-    <div id="map"></div>
-`);
