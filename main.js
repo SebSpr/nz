da7ec34c7b18126131338064da094fa197f33ac7
SebSpr <sebastian.spreitzer@uibk.ac.at>
2024-04-11 16:05:36 +0200

    lat long zoom

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-let map = L.map('map').setView([-43.53, 170.15], 13);
+const lat = -43.53;
+const long = 170.15;
+const zoom = 11;
+
+let map = L.map('map').setView([lat, long], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-let marker = L.marker([-43.53, 170.15]).addTo(map);
+let marker = L.marker([lat, long]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
