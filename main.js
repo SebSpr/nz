9fcf4f743774ecda4c9f99218e1025cd1fae3625
SebSpr <sebastian.spreitzer@uibk.ac.at>
2024-04-11 15:47:37 +0200

    test jss

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 var marker = L.marker([-43.53, 170.15]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+let absatz = "Hallo Welt";
+let nummer = 6;
+console.log(`
+    <h3> Test für's "Tutorial" </h3>
+    <p> ${absatz}</p>
+    <p> Nummer plus 1 = ${nummer +1} </p>
+    <div id="map"></div>
+`);
