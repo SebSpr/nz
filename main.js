1f97e1eb74f9528e24940b420971336efef40f92
SebSpr <sebastian.spreitzer@uibk.ac.at>
2024-04-11 17:15:11 +0200

    maßstab hinzugefügt

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let lat = -43.53;
-let long = 170.15;
+let lng = 170.15;
 let zoom = 11;
 
 
 // L = Leaflet
 
 let map = L.map('map', {
-    center: [lat, long],
+    center: [lat, lng],
     zoom: zoom
 });
 
@@ -14,7 +14,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-let marker = L.marker([lat, long]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
+L.control.scale({imperial: false, maxWidth: 100}).addTo(map);
 marker.bindPopup(`
     <h2>Fox Glacier</h2>
     <ul> 
